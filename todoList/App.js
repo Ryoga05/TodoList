9d8d5f1f134d29d4639e77ab710ba5dcd4819d70
Ryoga05 <ryoga20082005@gmail.com>
2024-11-20 08:31:13 +0100

    Cambios en el Navigator

--- todoList/App.js
+++ todoList/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './pages/TodoList';
+import TodoList from './pages/TodoList';
+import NewTodo from './pages/NewTodo';
 
 import { onAuthStateChanged } from 'firebase/auth';
 import { FIREBASE_AUTH } from './firebaseConfig';
@@ -12,7 +13,8 @@ const InsideStack = createNativeStackNavigator();
 function InsideLayout() {
   return (
     <InsideStack.Navigator>
-      <InsideStack.Screen name="Home" component={Home} options={{ headerShown: true, animation: 'none' }} />
+      <InsideStack.Screen name="TodoList" component={TodoList} options={{ headerShown: true, animation: 'none' }} />
+      <InsideStack.Screen name="NewTodo" component={NewTodo} options={{ headerShown: true, animation: 'none' }} />
     </InsideStack.Navigator>
   );
 }
