6bfdbeeb2d2cb64ccf01df2ff61895e249568916
Ryoga05 <ryoga20082005@gmail.com>
2024-11-20 08:30:16 +0100

    Primera subida

---
+++ todoList/App.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './pages/TodoList';
+
+import { onAuthStateChanged } from 'firebase/auth';
+import { FIREBASE_AUTH } from './firebaseConfig';
+
+const Stack = createNativeStackNavigator();
+const InsideStack = createNativeStackNavigator();
+
+function InsideLayout() {
+  return (
+    <InsideStack.Navigator>
+      <InsideStack.Screen name="Home" component={Home} options={{ headerShown: true, animation: 'none' }} />
+    </InsideStack.Navigator>
+  );
+}
---
+++ todoList/pages/TodoList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function TodoList() {
+    return (
+      <View style={styles.mainView}>
+        <View style={styles.tabView}>
+        </View>
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    mainView:
+    {
+        flex: 1,
+        backgroundColor: '#151723',
+    },
+    tabView:
+    {
+        flex: 7,
+        width: '100%',
+        height: '50%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    menuView:
+    {
+        flex: 1,
+    },
+  });
